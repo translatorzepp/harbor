7a109a80080f32cdc3916ea0bea0c7c8e5474338
zpalmer <zoe.palmer@getbraintree.com>
2018-02-16 10:44:52 -0600

    Generate random number stats

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 puts "http://localhost:8080/render?target=ship&from=-10min"
 loop do
   puts "We've just docked in the harbor."
-  `echo -n "ship:1|c" | nc -u -q1 0.0.0.0 8126`
+  ship = Random.rand(22)
+  `echo -n "ship:#{ship}|c" | nc -u -q1 0.0.0.0 8126`
   sleep(60)
 end
